9478580b42ee035f4422149e21a20d6ebaeeaa9f
Enis Mucaj <125411322+Enis777@users.noreply.github.com>
2024-12-07 19:37:22 +0100

    Delete movieshop10 directory

--- movieshop10/resources/slides.js
+++
@@ -1,6 +0,0 @@
-const slides = [
-    'https://iqq6kf0xmf.gjirafa.net/images/61d1ec7f-80ef-4d28-9e06-ca5c9e6d0386/61d1ec7f-80ef-4d28-9e06-ca5c9e6d0386.jpeg?w=1046',
-    'https://iqq6kf0xmf.gjirafa.net/images/bb949013-8ed6-4aa8-9104-e75963315a33/bb949013-8ed6-4aa8-9104-e75963315a33.jpeg?w=1046',
-    'https://iqq6kf0xmf.gjirafa.net/images/46387b01-f3af-4027-ab5c-92d38af7f90e/46387b01-f3af-4027-ab5c-92d38af7f90e.jpeg?w=1046',
-]
-export default slides
--- movieshop10/src/helpers/cart.js
+++
@@ -1,3 +0,0 @@
-export function exists(movie, movies) {
-    return movies.filter(m => m.id == movie.id).length > 0
-}
